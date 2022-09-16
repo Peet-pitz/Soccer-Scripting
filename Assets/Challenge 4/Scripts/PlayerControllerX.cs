ae50182587d841852558ec12d0b0816c8c22c6bb
Peter Njoroge <njoroge.pitz@gmail.com>
2022-09-16 11:35:07 +0300

    Update PlayerControllerX.cs
    Added a smoke particle feature but it's not working

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -15,7 +15,7 @@ public class PlayerControllerX : MonoBehaviour
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
     public ParticleSystem Smoke_Particle;
-
+    
 
 
     void Start()
